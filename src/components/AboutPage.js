7c3f9c2d932aba543997b864ad208fcd3a650641
narula98 <34324391+narula98@users.noreply.github.com>
2022-03-20 10:43:17 +0530

    1. Components created 2. basic navigation added(not react routes)

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+    render () {
+        return <React.Fragment><h1>About Page</h1>
+        <p>This app used React</p>
+        <a href="/home">Home</a>
+        </React.Fragment>
+    }
+}
+
+export default AboutPage;
