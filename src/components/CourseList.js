2001da431895b0c901f5fce78dca9181ca897165
narula98 <34324391+narula98@users.noreply.github.com>
2022-03-24 12:27:04 +0530

    Hooks added instead of lifecycle methods

---
+++ src/components/CourseList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function CourseList(props) {
+    return (
+        <>
+        <h2>Courses</h2>
+        <table className="table">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Author Id</th>
+                <th>Category</th>
+            </tr>
+            </thead>
+            <tbody>
+                {props.courses.map(course => {
+                    return <tr key={course.id}>
+                        <td>{course.title}</td>
+                        <td>{course.authorId}</td>
+                        <td>{course.category}</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+      </>
+    );
+}
+
+export default CourseList;
--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,41 +1,21 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
-class CoursePage extends Component {
-  state = {
-    courses: [],
-  };
+function CoursePage() {
+  const [courses, setCourses] = useState([]);
+  
+  useEffect( ()=>{
+    const courseApi = async ()=>{
+        const resp = await getCourses();
+        setCourses(resp);
+    }
+    courseApi();
+  }, []);
 
-  async componentDidMount() {
-    const resp = await getCourses();
-    this.setState({ courses: resp });
-  }
-
-  render() {
     return (
-      <div>
-        <h2>Courses</h2>
-        <table className="table">
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author Id</th>
-                <th>Category</th>
-            </tr>
-            </thead>
-            <tbody>
-                {this.state.courses.map(course => {
-                    return <tr key={course.id}>
-                        <td>{course.title}</td>
-                        <td>{course.authorId}</td>
-                        <td>{course.category}</td>
-                    </tr>
-                })}
-            </tbody>
-        </table>
-      </div>
+      <CourseList courses={courses}/>
     );
   }
-}
 
 export default CoursePage;
